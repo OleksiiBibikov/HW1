4bb821d73f84a08d4e967443b5cf51b27b8dea28
OleksiiBibikov <155468599+OleksiiBibikov@users.noreply.github.com>
2024-03-05 22:03:34 +0200

    git init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+
+            user1.Gender = Gender.Male;
+
+            Console.Write("Enter user first name: ");
+            user1.FirstName = Console.ReadLine();
+            Console.Write("Enter user last name: ");
+            user1.LastName = Console.ReadLine();
+
+            Console.WriteLine(user1.ToString());
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; private set; }
+        public Gender Gender { get; set; }
+
+        public User()
+        {
+            
+        }
+
+        public User(string firstName, string lastName, int age, Gender gender)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public override string ToString() 
+        {
+            return $"Hi, my name is {FirstName} and last name {LastName}. I am {20} years old. I am {Gender}.";
+        }
+    }
+}
